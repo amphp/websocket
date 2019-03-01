7d5b62b0d5c575aa484ae69cec56a13fd4f1f783
Aaron Piotrowski <aaron@trowski.com>
2019-02-28 23:28:49 -0600

    Move constant with functions
    Other libs drop constants into functions.php.

--- src/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Amp\Websocket;
-
-// defined in https://tools.ietf.org/html/rfc6455#section-4
-const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,9 @@
 
 namespace Amp\Websocket;
 
+// defined in https://tools.ietf.org/html/rfc6455#section-4
+const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+
 /**
  * @param int $length Random bytes to use to generate the key.
  *
