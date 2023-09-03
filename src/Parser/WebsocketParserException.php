ede65580c2cfb91b6feb8ff570020a9029ebf9b0
Aaron Piotrowski <aaron@trowski.com>
2023-09-03 14:00:53 -0500

    WebsocketParserException should extend WebsocketException

--- src/Parser/WebsocketParserException.php
+++ src/Parser/WebsocketParserException.php
@@ -2,7 +2,9 @@
 
 namespace Amp\Websocket\Parser;
 
-final class WebsocketParserException extends \Exception
+use Amp\Websocket\WebsocketException;
+
+final class WebsocketParserException extends WebsocketException
 {
     public function __construct(int $code, string $message)
     {
